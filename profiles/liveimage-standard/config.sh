18aa4d271ba488214ae744e60c5a1b139eab3d0b
xplshn <114888778+xplshn@users.noreply.github.com>
2024-09-29 23:24:42 -0300

    Update config.sh

--- profiles/liveimage-standard/config.sh
+++ profiles/liveimage-standard/config.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-enable_service connman
-enable_service greetd
+#enable_service connman
+#enable_service greetd
 
-cat <<EOF >>/etc/greetd/config.toml
-[initial_session]
-command = "bash -c 'cat /etc/motd && bash'"
-user = "ewe"
-EOF
+#cat <<EOF >>/etc/greetd/config.toml
+#[initial_session]
+#command = "bash -c 'cat /etc/motd && bash'"
+#user = "ewe"
+#EOF
